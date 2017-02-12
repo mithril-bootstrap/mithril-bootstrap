50725cdd2afbad088300a1b3a18081cee67ee3dd
Patrik Johnson <patrik@zebros.fi>
2017-02-12 17:23:00 +0200

    Project file structure, toolchain and util

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import rootImport  from 'rollup-plugin-root-import'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs'
+import buble       from 'rollup-plugin-buble'
+import filesize    from 'rollup-plugin-filesize'
+
+module.exports = {
+  entry      : 'index.js',
+  dest       : 'mithril-bootstrap.js',
+  format     : 'umd',
+  moduleId   : 'mithril-bootstrap',
+  moduleName : 'mithril-bootstrap',
+  external   : ['mithril'],
+  globals    : { mithril : 'm' },
+  plugins    : [
+    rootImport({ extensions: ['.js', '/index.js'] }),
+    nodeResolve({ jsnext: true, main: true, browser: true }),
+    commonjs(),
+    buble(),
+    filesize()
+  ]
+};
